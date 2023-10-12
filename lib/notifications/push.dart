1054050c0a40223db2fc85d1d965afb9dfc8a9cf
E-boi <54505527+E-boi@users.noreply.github.com>
2023-10-12 19:40:20 -0400

    [build] im dumb

--- lib/notifications/push.dart
+++ lib/notifications/push.dart
@@ -30,7 +30,7 @@ class PushNotifications {
       _flutterLocalNotificationsPlugin
           .resolvePlatformSpecificImplementation<
               AndroidFlutterLocalNotificationsPlugin>()
-          ?.requestPermission();
+          ?.requestNotificationsPermission();
     }
   }
 
